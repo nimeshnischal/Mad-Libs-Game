fb506dffa21c6c6df52e4d47bae21f115a542659
Nimesh_Nischal <n.nischal.1994@gmail.com>
2017-07-27 20:21:32 +0530

    Working crude demo of Mad Libs

---
+++ mad_libs.py
@@ -0,0 +1,47 @@
+from xlrd import open_workbook
+import nimesh_app_basics
+
+
+def get_story():
+    try:
+        wb = open_workbook('story_templates.xlsx')
+    except FileNotFoundError:
+        print("Stories not found. Please check if the file containing the stories exists or not.")
+        return -1
+    number_of_stories = wb.sheet_by_index(0).nrows
+    if number_of_stories == 1:
+        print("No story template found in the file. Please populate the file first.")
+        return -1
+    random_story_row_index = nimesh_app_basics.get_random_int_in_range(1, number_of_stories-1)
+    # TODO devise a mechanism to not repeat last attempted story
+    sheet = wb.sheet_by_index(0)
+    try:
+        return [sheet.cell(random_story_row_index, 1).value,
+                sheet.cell(random_story_row_index, 2).value], random_story_row_index
+    except ValueError:
+        print("There's some problem with story no. {0}. Please check the file for any problem."
+              .format(random_story_row_index))
+        return -1
+
+
+get_story_response = get_story()
+if get_story_response != -1:
+    try:
+        story_template = str(get_story_response[0][0])
+        blank_hints = str(get_story_response[0][1]).replace(" ", "").split(";")
+        story_index_in_file = get_story_response[1]
+        print(story_index_in_file)
+        blank_values = []
+        print("Enter the following inputs respective to each instructions:")
+        for hint in blank_hints:
+            blank_values.append(input(hint + " : "))
+            # TODO make a function in nimesh_app_basics input_non_empty_string
+        print(story_template.format(*blank_values))
+    except ValueError:
+        pass
+        # TODO print an error message and exit message
+    except IndexError:
+        print("A problem occurred. Please check the blank hints in file for story no. {0}.".format(story_index_in_file))
+else:
+    pass
+    # TODO print an error message and the exit message
---
+++ nimesh_app_basics.py
@@ -0,0 +1,63 @@
+import os
+import random
+
+
+# Function to clear the console screen
+def clear_screen():
+    clear = lambda:os.system('cls')
+    clear()
+
+
+def print_welcome_message(app_name):
+    welcome_message = " Welcome to {0}!! ".format(
+     "an App developed by Nimesh Nischal (aka NMZ)"
+        if app_name is None else app_name
+    ).center(100, "_")
+    print("\n", welcome_message, "\n")
+
+
+def get_int_input(input_message):
+    if input_message is None:
+        print("Please provide an integer input: ", end="")
+    else:
+        print(input_message, end="")
+    while True:
+        try:
+            return int(input())
+        except ValueError :
+            print("Invalid Input! Please try again: ", end="")
+            continue
+
+
+# Function to generate a random number between two numbers, including those two numbers
+def get_random_int_in_range(min_num, max_num):
+    return random.randint(min_num,max_num)
+
+
+def get_valid_y_n_opinion(input_message):
+    # Taking most of the possible yes no user inputs in consideration
+    user_choice_options = ['y', 'n', 'yes', 'no']
+    # Until now, no need to define user_choice_yes_options list
+    user_choice_no_options = ['n', 'no']
+    if input_message is None:
+        print("Please provide an input: ( y / n ): ", end="")
+    else:
+        print(input_message, end="")
+    while True:
+        user_choice = input()
+        if user_choice.lower() not in user_choice_options:
+            print("Wrong input. Please try again ( y / n ): ", end="")
+            continue
+        break
+    if user_choice.lower() in user_choice_no_options:
+        return False
+    else:
+        return True
+
+
+def print_exit_message(app_name):
+    exit_message = " Thank you for using {0}. Developed by Nimesh Nischal (aka NMZ). ".format(
+        "this app" if app_name is None else app_name
+    ).center(100, "-")
+    print("\n", exit_message, "\n")
+    input("Press enter to exit!")
